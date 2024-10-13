edc4433904e56004f105e5aae32713a813c43469
Jeremias Vallejos <vallejosjeremias14@gmail.com>
2024-10-13 13:29:42 -0300

    fix: add relations

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ const express = require("express");
 const app = express();
 const router = require('./routes/routes')
 const db = require("./conexion/database");
+const Contenido = require("./models/contenido");
+const Genero = require("./models/genero");
+const ContenidoGenero = require("./models/contenido-genero");
 
 // Middlewares
+
+Contenido.belongsToMany(Genero, { through: ContenidoGenero, foreignKey: 'ContenidoId' });
+Genero.belongsToMany(Contenido, { through: ContenidoGenero, foreignKey: 'GeneroId' });
+
 app.use(express.json());
 app.use("/contenido", router);
 
